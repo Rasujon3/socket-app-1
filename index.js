a8d81c41a0914a940fc92c79acfc7c1574e35772
Sujon Office <>
2026-01-08 12:35:00 +0600

    socket.io setup

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
+const http = require("http");
+const expressServer = http.createServer(app);
 const port = 3000;
+
+const { Server } = require("socket.io");
+const io = new Server(expressServer);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(port, () => {
+expressServer.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
